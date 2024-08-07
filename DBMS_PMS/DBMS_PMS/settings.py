3a0ec1a176329db504a85b0070c75c36dbb8a3da
Bilal-Sheikh-30 <bilalsheikh3011@gmail.com>
2024-08-07 15:16:11 +0500

    configured settings for vercel

--- DBMS_PMS/DBMS_PMS/settings.py
+++ DBMS_PMS/DBMS_PMS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-70g9hk31jk_by0h37hd*u--)ahge58^%6kf(p5!zjno=1miag8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
